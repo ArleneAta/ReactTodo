3d9faec7849c16805e2b56685ee21977e9f3f38b
Arlene Ata <arlene.ata@gmail.com>
2019-09-11 21:15:15 -0700

    initial

---
+++ src/Components/Todos.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Todos extends Component{
+  render(){
+      console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+        
+        <h3> {this.title} </h3>
+    ));
+  }
+}
+  
+
+
+export default Todos;
